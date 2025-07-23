a1d35c2db37812c6df9a30e8e47389bbd65877ae
Alexey Yazadzhi <alexdragh.09@gmail.com>
2025-07-23 15:29:52 +0300

    Добавлена стилизация для index-mobile

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,10 @@
-/* в этот файл добавляет скрипты*/
+const header = document.querySelector('.header');
+const headerNavToggle = document.querySelector('.header__nav-toggle');
+
+headerNavToggle.addEventListener('click', () => {
+  if (header.classList.contains('header--nav-closed')){
+    header.classList.remove('header--nav-closed');
+  } else {
+    header.classList.add('header--nav-closed');
+  }
+});
