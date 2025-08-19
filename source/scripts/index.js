54d44056b38dd2e4c49b34930fadcf86003e3a85
Alexey Yazadzhi <alexdragh.09@gmail.com>
2025-08-19 17:56:09 +0300

    Додалана вся адаптивность

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -4,7 +4,11 @@ const headerNavToggle = document.querySelector('.header__nav-toggle');
 headerNavToggle.addEventListener('click', () => {
   if (header.classList.contains('header--nav-closed')){
     header.classList.remove('header--nav-closed');
+    headerNavToggle.classList.remove('button--burger');
+    headerNavToggle.classList.add('button--x');
   } else {
     header.classList.add('header--nav-closed');
+    headerNavToggle.classList.add('button--burger');
+    headerNavToggle.classList.remove('button--x');
   }
 });
