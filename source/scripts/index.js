e4635b56139b9908fa000980054808d8d798939d
Far4ich <79937711+Far4ich@users.noreply.github.com>
2025-08-19 17:59:11 +0300

    Module7 (#7)
    * Добавлена стилизация для index-mobile
    
    * Добавлен index tablet
    
    * Добален index desktop
    
    * Додалана вся адаптивность

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,14 @@
-/* в этот файл добавляет скрипты*/
+const header = document.querySelector('.header');
+const headerNavToggle = document.querySelector('.header__nav-toggle');
+
+headerNavToggle.addEventListener('click', () => {
+  if (header.classList.contains('header--nav-closed')){
+    header.classList.remove('header--nav-closed');
+    headerNavToggle.classList.remove('button--burger');
+    headerNavToggle.classList.add('button--x');
+  } else {
+    header.classList.add('header--nav-closed');
+    headerNavToggle.classList.add('button--burger');
+    headerNavToggle.classList.remove('button--x');
+  }
+});
